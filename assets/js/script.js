3bbe5f22e2c1eb3a9d71e64580f52c29002e9836
Davi Lima <davi.n.lima99@gmail.com>
2020-04-13 22:24:02 -0300

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,14 @@ const mongoBar = document.querySelector('.bar-mongodb');
 var t1 = new TimelineLite()
 
 t1.fromTo(phpBar, .70, {width: `calc(0% - 6px)`}, {width: `calc(86% - 6px)`, height: `20px`, ease: Power4.easeOut})
-    .fromTo(javaBar, .70, {width: `calc(0% - 6px)`}, {width: `calc(70% - 20px)`, height: `20px`, ease: Power4.easeOut})
+    .fromTo(javaBar, .70, {width: `calc(0% - 6px)`}, {width: `calc(68% - 6px)`, height: `20px`, ease: Power4.easeOut})
     .fromTo(nodeBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(60% - 6px)`, height: `20px`, ease: Power4.easeOut})
     .fromTo(springBar, .57, {width: `calc(0% - 6px)`}, {width: `calc(50% - 6px)`, height: `20px`, ease: Power4.easeOut})
     .fromTo(laravelBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(79% - 6px)`, height: `20px`, ease: Power4.easeOut})
-    .fromTo(mysqlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, height: `20px`, ease: Power4.easeOut})
-    .fromTo(mongoBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(65% - 6px)`, height: `20px`, ease: Power4.easeOut})
+    .fromTo(mysqlBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(82% - 6px)`, height: `20px`, ease: Power4.easeOut})
+    .fromTo(mongoBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(50% - 6px)`, height: `20px`, ease: Power4.easeOut})
     .fromTo(symfonyBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(72% - 6px)`, height: `20px`, ease: Power4.easeOut})
-    .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(60% - 6px)`, height: `20px`, ease: Power4.easeOut});
+    .fromTo(reactBar, .75, {width: `calc(0% - 6px)`}, {width: `calc(63% - 6px)`, height: `20px`, ease: Power4.easeOut});
 
 const controller = new ScrollMagic.Controller();
 
